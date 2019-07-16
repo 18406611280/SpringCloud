b80c33d849cb1a3d7f6775ae13eb1205ff6deb01
hongye <823169071@qq.com>
2019-07-17 01:05:30 +0800

    feign commit

--- cloud-feign/src/main/java/blueKite/com/feign/FeignMember.java
+++ cloud-feign/src/main/java/blueKite/com/feign/FeignMember.java
@@ -2,10 +2,12 @@ package blueKite.com.feign;
 
 import java.util.List;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @FeignClient(value="member",fallback=HystrixService.class)
+@Component
 public interface FeignMember {
 	
 	@RequestMapping("/getMemberList")
